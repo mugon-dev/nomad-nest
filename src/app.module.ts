e833612b6524a02b070be4955465e2ebab0a1093
jmg <jmg41490400@gmail.com>
2022-05-20 18:20:07 +0900

    class validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoivesModule } from './movies/moives.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  imports: [MoivesModule],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //유효성 검사용 파이프
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      // 비정상적인 요청 체크
+      forbidNonWhitelisted: true,
+      // 원하는 타입으로 자동으로 변경
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
+
 bootstrap();
